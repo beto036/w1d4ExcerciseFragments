97158f7d80a468ee6538f2b0acf9afef9bc76f68
beto036 <beto.es3@gmail.com>
2016-10-21 17:01:00 -0400

    First commit

---
+++ app/src/main/java/com/example/admin/w1d4excercisefragments/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.admin.w1d4excercisefragments;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
